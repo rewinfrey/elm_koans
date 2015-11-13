1fa4593d6d2868fa8bae620ceaa21b38e198c956
Rick Winfrey <rick.winfrey@gmail.com>
2015-11-13 16:39:45 -0600

    Answers AboutAsserts koans

--- src/Koans/AboutAsserts.elm
+++ src/Koans/AboutAsserts.elm
@@ -9,9 +9,9 @@ testSuite =
   suite
     "About Asserts"
     [ test "assert tests for a true value"
-        (assert xBool)
+        (assert True)
     , test "assertEqual tests for equality"
-        (assertEqual True xBool)
+        (assertEqual True True)
     , test "assertNotEqual tests for inequality"
-        (assertNotEqual False xBool)
+        (assertNotEqual False True)
     ]
