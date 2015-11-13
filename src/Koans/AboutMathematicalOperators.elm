49480578d0eaa8a502250410e503498cc057426d
Rick Winfrey <rick.winfrey@gmail.com>
2015-11-13 16:58:34 -0600

    Answers AboutMathematicalOperators koans

--- src/Koans/AboutMathematicalOperators.elm
+++ src/Koans/AboutMathematicalOperators.elm
@@ -9,31 +9,31 @@ testSuite =
   suite
     "About Mathematical Operators"
     [ test "negate gives the negative of a number"
-        (assertEqual -3 (negate xNum))
+        (assertEqual -3 (negate 3))
     , test "abs gives the absolute value of a number"
-        (assertEqual 3 (abs xNum))
+        (assertEqual 3 (abs (negate 3)))
     , test "sqrt gives the square root of a Float"
-        (assertEqual 3.0 (sqrt xNum))
+        (assertEqual 3.0 (sqrt 9.0))
     , test "+ adds numbers"
-        (assertEqual 3 (1 + xNum))
+        (assertEqual 3 (1 + 2))
     , test "- subtracts numbers"
-        (assertEqual 3 (7 - xNum))
+        (assertEqual 3 (7 - 4))
     , test "* multiplies numbers"
-        (assertEqual 6 (4 * xNum))
+        (assertEqual 6 (3 * 2))
     , test "/ divides Floats"
-        (assertEqual 2.5 (5 / xNum))
+        (assertEqual 2.5 (5 / 2.0))
     , test "// divides Ints"
-        (assertEqual 2 (5 // xNum))
+        (assertEqual 2 (5 // 2))
     , test "rem gives the remainder after division"
-        (assertEqual 2 (5 `rem` xNum))
+        (assertEqual 2 (5 `rem` 3))
     , test "% performs modular arithmetic (which is different)"
-        (assertEqual xNum (-1 % 5))
+        (assertEqual 4 (-1 % 5))
     , test "logBase returns the log of the value in the given base"
-        (assertEqual xNum (logBase 2 8))
+        (assertEqual 3 (logBase 2 8))
     , test "clamp returns the value if it is between the given min and max"
-        (assertEqual xNum (clamp 1 10 5))
+        (assertEqual 5 (clamp 1 10 5))
     , test "clamp returns the min value if the value is below the minimum"
-        (assertEqual xNum (clamp 1 10 -5))
+        (assertEqual 1 (clamp 1 10 -5))
     , test "clamp returns the max value if the value is above the maximum"
-        (assertEqual xNum (clamp 1 10 50))
+        (assertEqual 10 (clamp 1 10 50))
     ]
