b9ab279afeadb73e39b92e73a891dcb593825247
Rick Winfrey <rick.winfrey@gmail.com>
2015-11-13 20:25:01 -0600

    Answers AboutTuples koan

--- src/Koans/AboutTuples.elm
+++ src/Koans/AboutTuples.elm
@@ -9,21 +9,21 @@ testSuite =
   suite
     "About Tuples"
     [ test "tuples are like lists of fixed length"
-        (assertEqual xTuple (1, 2))
+        (assertEqual (1, 2) (1, 2))
     , test "tuples may also be of mixed types"
-        (assertEqual xTuple2 (1, "hey"))
+        (assertEqual (1, "hey") (1, "hey"))
     , test "there is a special comma syntax for creating tuples"
-        (assertEqual xTuple2 ((,) 1 "hey"))
+        (assertEqual (1, "hey") ((,) 1 "hey"))
     , test "you use as many commas as there would be in the tuple"
-        (assertEqual xTuple3 ((,,) 1 "hey" []))
+        (assertEqual (1, "hey", []) ((,,) 1 "hey" []))
     , test "fst gets the first element of a 2-tuple"
-        (assertEqual xNum (fst xTuple2))
+        (assertEqual 100 (fst ((,) 100 1000)))
     , test "snd gets the second element of a 2-tuple"
-        (assertEqual xString (snd xTuple2))
+        (assertEqual "thar" (snd ((,) "hello" "thar")))
     , test "case statements may be used to destructure a tuple"
         (case (1, 2) of
           (first, second) ->
-            (assert ((first == xNum) && (second == xNum))))
+            (assert ((first == 1) && (second == 2))))
     , test "tuples may also be destructured by function arguments"
-        (assert ((\(f, s) -> ((f == xNum) && (s == xNum))) (1, 2)))
+        (assert ((\(f, s) -> ((f == 1) && (s == 2))) (1, 2)))
     ]
